ea26bd78d3b235b66e802c564422b4bb8646f42c
Bingxin <bingxin@baka.pub>
2026-07-05 14:44:28 +0000

    revert: restore rich header behavior from f6a5b26

--- main/src/services/ForwardService.ts
+++ main/src/services/ForwardService.ts
@@ -560,8 +560,7 @@ export default class ForwardService {
         message += `\n<b>@${this.tgBot.me.username}</b>`;
       }
 
-      let richHeaderUsed = false,
-        richHeaderPreviewUsed = false;
+      let richHeaderUsed = false;
       if (((pair.flags | this.instance.flags) & flags.DISABLE_RICH_HEADER) || !env.WEB_ENDPOINT) {
         messageHeaderWithLink = messageHeader;
       }
@@ -579,7 +578,6 @@ export default class ForwardService {
         && (isContainAtOrChannelFace || !isContainsUrl(message))) {
         // 没有文件时才能显示链接预览
         richHeaderUsed = true;
-        richHeaderPreviewUsed = true;
         // https://github.com/tdlib/td/blob/437c2d0c6e0ad104022d5ad86ddc8aedc41cb7a8/td/telegram/MessageContent.cpp#L2575
         // https://github.com/tdlib/td/blob/437c2d0c6e0ad104022d5ad86ddc8aedc41cb7a8/td/generate/scheme/telegram_api.tl#L1841
         // https://github.com/gram-js/gramjs/pull/633
@@ -604,9 +602,8 @@ export default class ForwardService {
         }
       }
 
-      if (!richHeaderUsed || richHeaderPreviewUsed) {
-        const fallbackHeader = richHeaderPreviewUsed ? messageHeader : messageHeaderWithLink;
-        message = fallbackHeader + (message && fallbackHeader ? '\n' : '') + message;
+      if (!richHeaderUsed) {
+        message = messageHeaderWithLink + (message && messageHeaderWithLink ? '\n' : '') + message;
       }
       message && (messageToSend.message = message);
 
@@ -623,10 +620,9 @@ export default class ForwardService {
           this.log.warn('Rich Header 发送错误', messageToSend.file, e);
           posthog.capture('Rich Header 发送错误', { error: e, attach: messageToSend.file });
           delete messageToSend.file;
-          messageToSend.linkPreview = false;
-          if (messageHeaderWithLink && message.startsWith(messageHeaderWithLink)) {
-            message = messageHeader + message.substring(messageHeaderWithLink.length);
-          }
+          delete messageToSend.linkPreview;
+          // 这里可能是因为 url 本身不合法之类的问题，所以这里发送不带链接的 header
+          message = messageHeader + (message && messageHeader ? '\n' : '') + message;
           message && (messageToSend.message = message);
           tgMessage = await pair.tg.sendMessage(messageToSend);
         }
